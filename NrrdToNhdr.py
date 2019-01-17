4cebd741b48d7f70e3541709bb306620fd3afb26
Billah, Tashrif <tbillah@bwh.harvard.edu>
2019-01-17 13:26:40 -0500

    nrrd to nhdr

---
+++ NrrdToNhdr.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import nrrd
+from plumbum import cli
+
+class App(cli.Application):
+
+    force = cli.Flag(['--force'], help='Force overwrite if output already exists',mandatory=False,default=False)
+    threshold = cli.SwitchAttr('-f', help='Bet fractional intensity threshold', default=0.1)
+
+    nrrdFile = cli.SwitchAttr(
+        ['-i', '--input'],
+        cli.ExistingFile,
+        help='input nrrd',
+        mandatory=True)
+
+    nhdrFile = cli.SwitchAttr(
+        ['-o', '--out'],
+        cli.NonexistentPath,
+        help='output nhdr',
+        mandatory=True)
+
+    def main(self):
+
+        img= nrrd.read(self.nrrdFile)
+
+        nrrd.write(self.nhdrFile, img[0], header=img[1], compression_level = 1)
+
+
+if __name__ == '__main__':
+    App.run()
