cba240c5fe4ad11e0f548f35e22dfb7928c43e36
Billah, Tashrif <tbillah@bwh.harvard.edu>
2019-03-05 10:15:20 -0500

    ants wrapper

---
+++ antsUtil.py
@@ -0,0 +1,30 @@
+from subprocess import check_call
+from plumbum.cmd import antsApplyTransforms
+from plumbum import FG
+
+def antsReg(fixedImg, fixedMask, movingImg, outPrefix):
+    if fixedMask:
+        check_call((' ').join(['antsRegistrationSyNQuick.sh',
+                               '-d', '3',
+                               '-f', fixedImg,
+                               '-x', fixedMask,
+                               '-m', movingImg,
+                               '-o', outPrefix,
+                               '-n', '8']), shell=True)
+    else:
+        check_call((' ').join(['antsRegistrationSyNQuick.sh',
+                               '-d', '3',
+                               '-f', fixedImg,
+                               '-m', movingImg,
+                               '-o', outPrefix,
+                               '-n', '8']), shell=True)
+
+
+def applyXform(inImg, refImg, warp, trans, outImg):
+    antsApplyTransforms[
+        '-d', '3',
+        '-i', inImg,
+        '-o', outImg,
+        '-r', refImg,
+        '-t', warp, trans
+    ] & FG
