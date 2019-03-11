aadc129a59a753289a1bbeee9de7b18939193564
Billah, Tashrif <tbillah@bwh.harvard.edu>
2019-03-11 14:31:01 -0400

    test data download path

--- setup.py
+++ setup.py
@@ -1,18 +1,21 @@
 from setuptools import setup, find_packages
+from os.path import join as pjoin
+from glob import glob
 
 requirements= open('requirements.txt').read().split()
-print(requirements)
+# print(requirements)
 
 setup(name='conversion',
-    version='1.0',
+    version='2.0',
     description='Python utility scripts for MRI processing',
     author='Tashrif Billah',
-    author_email='tb571@bwh.harvard.edu, tashrifbillah@gmail.com',
+    author_email='tbillah@bwh.harvard.edu, tashrifbillah@gmail.com',
     url='https://github.com/pnlbwh/conversion',
     license='MIT License',
     install_requires=requirements,
     packages=find_packages(exclude=('*tests*',)),
-    package_data={'tests':'tests/*', 'data':'data/*'},
+    package_data={'conversion':[pjoin('tests','*'), pjoin('tests','data','*'), pjoin('data','*')]},
+    data_files=[('share/doc/conversion/data',glob(pjoin('data','*')))],
     project_urls={'Tracker':'https://github.com/pnlbwh/conversion/issues',
-                    'Documentation':'https://github.com/pnlbwh/conversion/README.md'},
-    python_requires='>=3.0')
+                   'Documentation':'https://github.com/pnlbwh/conversion/README.md'},
+    python_requires='>=3')
