25af24c5fc735ca269f43084c7544c8021ee9c52
Billah, Tashrif <tbillah@bwh.harvard.edu>
2019-03-11 14:21:32 -0400

    needed for package discovery

---
+++ conversion/__init__.py
@@ -0,0 +1,10 @@
+from conversion._version import __version__
+from conversion.bval_bvec_io import read_bvals, read_bvecs, write_bvals, write_bvecs, \
+    bvec_scaling, nrrd_bvals_bvecs, transpose, read_grad_ind
+# from conversion import bval_bvec_io
+from conversion.nhdr_write import nhdr_write
+from conversion.nifti_write import nifti_write
+from conversion.grad_remove import grad_remove
+from conversion.grad_avg import grad_avg
+from conversion.fs_label_parser import parse_labels
+# from conversion import fs_label_parser
