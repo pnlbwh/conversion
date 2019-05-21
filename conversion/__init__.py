9c8e70ecc5f0b67bc5459c2ee6888d2c3ce88417
Billah, Tashrif <tbillah@bwh.harvard.edu>
2019-05-21 15:14:02 +0000

    read caselist util functions

--- conversion/__init__.py
+++ conversion/__init__.py
@@ -7,4 +7,4 @@ from conversion.nifti_write import nifti_write
 from conversion.grad_remove import grad_remove
 from conversion.grad_avg import grad_avg
 from conversion.fs_label_parser import parse_labels
-from conversion.util import read_caselist, num2str
+from conversion.util import read_imgs, read_imgs_masks, read_cases, loadExecutable, num2str
